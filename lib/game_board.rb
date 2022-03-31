813720ee11c8f97c93722dd5f666722746b3970b
Colin Reinhart <colinbreinhartgmail.com>
2022-03-31 12:44:04 -0600

    Storing just cells in game_board

--- lib/game_board.rb
+++ lib/game_board.rb
@@ -1,19 +1,71 @@
-require './spec/player.rb'
+# require './lib/player.rb'
+require 'pry'
 
 class GameBoard
-  attr_reader :board
+  attr_reader :cells
 
-  def initialize(board)
-    @board = board
+  def initialize
+    @cells = {
+      a1: '.',
+      a2: '.',
+      a3: '.',
+      a4: '.',
+      a5: '.',
+      a6: '.',
+      b1: '.',
+      b2: '.',
+      b3: '.',
+      b4: '.',
+      b5: '.',
+      b6: '.',
+      c1: '.',
+      c2: '.',
+      c3: '.',
+      c4: '.',
+      c5: '.',
+      c6: ".",
+      d1: '.',
+      d2: '.',
+      d3: '.',
+      d4: '.',
+      d5: '.',
+      d6: '.',
+      e1: '.',
+      e2: '.',
+      e3: '.',
+      e4: '.',
+      e5: '.',
+      e6: '.',
+      f1: '.',
+      f2: '.',
+      f3: '.',
+      f4: '.',
+      f5: '.',
+      f6: '.',
+      g1: '.',
+      g2: '.',
+      g3: '.',
+      g4: '.',
+      g5: '.',
+      g6: '.',
+    }
   end
 
-  #Use hashes to assign the key value pair, key = row#, value = '.', 'X', or 'O'
-  def print_board
-    p "A B C D E F G"
-    p (row6, A) (row6, B) (row6, C) (row1, D) (row1, E) (row1, F) (row1, G)
-    p (row5, A) (row5, B) (row5, C) (row1, D) (row1, E) (row1, F) (row1, G)
-    p (row4, A) (row4, B) (row4, C) (row1, D) (row1, E) (row1, F) (row1, G)
-    p (row3, A) (row3, B) (row3, C) (row1, D) (row1, E) (row1, F) (row1, G)
-    p (row2, A) (row2, B) (row2, C) (row1, D) (row1, E) (row1, F) (row1, G)
-    p (row1, A) (row1, B) (row1, C) (row1, D) (row1, E) (row1, F) (row1, G)
-  end
+
+  # def cells
+  #
+  # end
+  #
+  #
+  # def print_board
+  #   puts "A B C D E F G"
+  #   puts (cells[:a6])#, cells[:b6], cells[:c6], cells[:d6], cells[:e6], cells[:f6], cells[:g6])
+  #   # p cells[:a5] cells[:b5] cells[:c5] cells[:d5] cells[:e5] cells[:f5]cells[:g5]
+  #   # p cells[:a4] cells[:b4] cells[:c4] cells[:d4] cells[:e4] cells[:f4]cells[:g4]
+  #   # p cells[:a3] cells[:b3] cells[:c3] cells[:d3] cells[:e3] cells[:f3]cells[:g3]
+  #   # p cells[:a2] cells[:b2] cells[:c2] cells[:d2] cells[:e2] cells[:f2]cells[:g2]
+  #   # p cells[:a1] cells[:b1] cells[:c1] cells[:d1] cells[:e1] cells[:f1]cells[:g1]
+  # end
+  #
+  # # return print_board
+end
