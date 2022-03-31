c6610d662726738af2384a51e74efc9dd95f72c0
Colin Reinhart <colinbreinhartgmail.com>
2022-03-31 12:43:00 -0600

    moved board output to connect_four

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,23 +1,41 @@
-class ConnectFour
-  attr_reader :board
+require './lib/game_board'
+# class ConnectFour
+#   attr_reader :puts_board
+#
+#   def initialize
+#     @puts_board = puts_board
+#   end
+#
+#   def welcome
+    p "Welcome to the Kribs-Reinhart Connect 4"
+    p "In this game you will play against an unintelegent computer"
+    p "See if you can connect 4 before the Dumbputer does"
+    # Maybe put a gets.chomp in here for player name?
+    p "Type 'GO' to start the game"
+    p ">>>"
+    input = gets.chomp.upcase
+    if input == "GO"
+      game = Game.new
+      game.puts_board
+    else
+      p "Fine, don't play our game"
+    end
+  # end
 
-  def initialize(board)
-    @board = board
+  def puts_board
+    puts "A B C D E F G"
+    print cells[:a1], " ", cells[:a2], " ", cells[:a3], " ", cells[:a4], " ", cells[:a5], " ", cells[:a6], " ", cells[:b1]
+    puts ''
+    print cells[:a1], " ", cells[:a2], " ", cells[:a3], " ", cells[:a4], " ", cells[:a5], " ", cells[:a6], " ", cells[:b1]
+    puts ''
+    print cells[:a1], " ", cells[:a2], " ", cells[:a3], " ", cells[:a4], " ", cells[:a5], " ", cells[:a6], " ", cells[:b1]
+    puts ''
+    print cells[:a1], " ", cells[:a2], " ", cells[:a3], " ", cells[:a4], " ", cells[:a5], " ", cells[:a6], " ", cells[:b1]
+    puts ''
+    print cells[:a1], " ", cells[:a2], " ", cells[:a3], " ", cells[:a4], " ", cells[:a5], " ", cells[:a6], " ", cells[:b1]
+    puts ''
+    print cells[:a1], " ", cells[:a2], " ", cells[:a3], " ", cells[:a4], " ", cells[:a5], " ", cells[:a6], " ", cells[:b1]
+
   end
 
-def welcome
-  p "Welcome to the Kribs-Reinhart Connect 4"
-  p "In this game you will play against an unintelegent computer"
-  p "See if you can connect 4 before the Dumbputer does"
-  # Maybe put a gets.chomp in here for player name?
-  p "Type 'GO' to start the game"
-  p ">>>"
-  input = gets.chomp.upcase
-  if input == "GO"
-    game = Game.new
-    game.start
-  else
-    p "Fine, don't play our game"
-  end
-end
-welcome
+# end
